e33727d30f69d220bd60f2850b96fc5cfb0e83c0
xdanigwn <67907512+xdanigwn@users.noreply.github.com>
2020-10-07 23:35:20 +0700

    add sbadmin2

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(
   "/sb-admin-2",
-  express.static(path.join(__dirname, "node_modules/startbootstrap-sb-admin-2"))
+  express.static(path.join(__dirname, "public/stylesheets/startbootstrap-sb-admin-2"))
 );
 
 app.use("/", indexRouter);
