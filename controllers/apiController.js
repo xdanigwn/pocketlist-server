66ac06a97cc747812c0d2f8bfddd30214c2eff59
danigwn <danigwn@gmail.com>
2020-12-30 19:03:14 +0700

    pocketlist-server

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -155,7 +155,7 @@ module.exports = {
           });
 
           const account = await Account.findOne({ _id: accountId });
-          account.balance += ammount;
+          account.balance += parseInt(ammount);
           console.log(account);
   
           await account.save();
