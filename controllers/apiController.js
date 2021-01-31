2a54b7a5205251c1b9645ed01f9e76208ed6fd80
danigwn <danigwn@gmail.com>
2021-02-01 02:01:25 +0700

    pocketlist-server

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -202,7 +202,7 @@ module.exports = {
           return res.status(200).json({ message: "Success Submit" });
         }else if (category.ctgType === "Transfer") {
 
-          console.log(userId)
+          console.log(transDate)
 
           // await Trans.create({
           //   transDate,
