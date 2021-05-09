ccd35e15dd3a8e39ef18496f08dafa373ec7e685
Danigwn <danigwn@gmail.com>
2021-05-09 12:29:43 +0700

    update 3

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ const apiRouter = require("./routes/api");
 var app = express();
 
 app.use(cors({
-  origin : ["http://localhost:3001" , "https://app-pocketlist.herokuapp.com"],
+  origin : ["https://app-pocketlist.herokuapp.com"],
   credentials : true 
 }));
 
