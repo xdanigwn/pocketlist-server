b10e1a4b5d849c9a1ae8bd2fbd4c4c27684a4343
Danigwn <danigwn@gmail.com>
2021-05-09 12:42:50 +0700

    update 3

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -170,7 +170,7 @@ module.exports = {
       // console.log(token)
       res.cookie("token", token, {
         domain : "herokuapp.com",
-        httpOnly : true,
+        // httpOnly : true,
         // secure: true,
         // sameSite : 'none',
         // secure: req.secure
