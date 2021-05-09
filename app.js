cfaa557d9e25777409b0db04e19bd5fcfd747c65
Danigwn <danigwn@gmail.com>
2021-05-10 01:58:53 +0700

    update 4

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.use(
     secret: "keyboard cat",
     resave: false,
     saveUninitialized: true,
-    cookie: { maxAge: 100000, sameSite:'none', secure:true },
+    cookie: { maxAge: 100000 },
   })
 );
 app.use(flash());
