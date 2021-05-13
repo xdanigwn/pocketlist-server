058bfb1b0736f0c99c79bfc9cb29eabfd78e44d7
danigwn <danigwn@gmail.com>
2021-05-14 00:48:11 +0700

    update 6

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -115,7 +115,7 @@ module.exports = {
       res.setHeader("Access-Control-Allow-Origin", "https://app-pocketlist.herokuapp.com")
       res.setHeader("Access-Control-Allow-Credentials", "true");
       res.setHeader("Access-Control-Max-Age", "1800");
-      res.setHeader("Access-Control-Allow-Headers", "content-type");
+      res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
       res.setHeader("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
 
       const token = req.cookies.token; // cookie parser
@@ -152,7 +152,7 @@ module.exports = {
       res.setHeader("Access-Control-Allow-Origin", "https://app-pocketlist.herokuapp.com")
       res.setHeader("Access-Control-Allow-Credentials", "true");
       res.setHeader("Access-Control-Max-Age", "1800");
-      res.setHeader("Access-Control-Allow-Headers", "content-type");
+      res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
       res.setHeader("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
 
       // // res.json({ msg : "hello"})
